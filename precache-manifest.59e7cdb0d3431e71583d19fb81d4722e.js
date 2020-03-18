0f08d45f91fc8add4766f407012e014995733b30
Shigeaki Shimizu <shgkshimizu@gmail.com>
2020-03-19 00:50:33 +0800

    Updates

---
+++ precache-manifest.59e7cdb0d3431e71583d19fb81d4722e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64a68bb439d92a375f716108b491b5ea",
+    "url": "/covid19phstats/index.html"
+  },
+  {
+    "revision": "42cef7390fb4392243e7",
+    "url": "/covid19phstats/static/css/main.4c6f2058.chunk.css"
+  },
+  {
+    "revision": "35e9f42304e455e4aa4e",
+    "url": "/covid19phstats/static/js/2.8ff639f5.chunk.js"
+  },
+  {
+    "revision": "7f7b7554ad8c1d8206ae278b825bcae0",
+    "url": "/covid19phstats/static/js/2.8ff639f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42cef7390fb4392243e7",
+    "url": "/covid19phstats/static/js/main.4ab05c63.chunk.js"
+  },
+  {
+    "revision": "b1da1d59b51cc881439e",
+    "url": "/covid19phstats/static/js/runtime-main.461b312f.js"
+  }
+]);

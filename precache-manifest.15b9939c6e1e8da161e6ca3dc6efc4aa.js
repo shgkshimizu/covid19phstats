fbfe355b6297d7057acd82b6fa741dd20255c609
Shigeaki Shimizu <shgkshimizu@gmail.com>
2020-04-01 15:07:41 +0800

    Updates

--- precache-manifest.15b9939c6e1e8da161e6ca3dc6efc4aa.js
+++ precache-manifest.15b9939c6e1e8da161e6ca3dc6efc4aa.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "86347ae31a6cc2582d5295a03292c496",
+    "revision": "77d5a4516768566db1fc5145a1592e9f",
     "url": "/covid19phstats/index.html"
   },
   {
-    "revision": "5130d05a840e74e3f12e",
+    "revision": "62fe94e177a828ae805d",
     "url": "/covid19phstats/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "743093c6f1d435a20aac",
+    "revision": "59960459ed80bea3ab5c",
     "url": "/covid19phstats/static/css/main.bcf1954d.chunk.css"
   },
   {
-    "revision": "5130d05a840e74e3f12e",
-    "url": "/covid19phstats/static/js/2.a68ab7c9.chunk.js"
+    "revision": "62fe94e177a828ae805d",
+    "url": "/covid19phstats/static/js/2.a6684a32.chunk.js"
   },
   {
     "revision": "18e4c7d63bfbf17f55f88bea26ef42f5",
-    "url": "/covid19phstats/static/js/2.a68ab7c9.chunk.js.LICENSE.txt"
+    "url": "/covid19phstats/static/js/2.a6684a32.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "743093c6f1d435a20aac",
-    "url": "/covid19phstats/static/js/main.db7d0ee1.chunk.js"
+    "revision": "59960459ed80bea3ab5c",
+    "url": "/covid19phstats/static/js/main.89c92903.chunk.js"
   },
   {
     "revision": "b1da1d59b51cc881439e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19phstats/precache-manifest.57dca72647c6b4a65b53189f45224d9c.js"
+  "/covid19phstats/precache-manifest.15b9939c6e1e8da161e6ca3dc6efc4aa.js"
 );
 
 self.addEventListener('message', (event) => {
